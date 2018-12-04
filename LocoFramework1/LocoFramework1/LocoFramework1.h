dfd046218a7a58f6353afdd53a5f582616e352d6
Krishna Mohan <krishnamohan@Krishnas-MacBook-Pro.local>
2018-12-04 23:19:30 +0530

    Finished

---
+++ LocoFramework1/LocoFramework1/LocoFramework1.h
@@ -0,0 +1,19 @@
+//
+//  LocoFramework1.h
+//  LocoFramework1
+//
+//  Created by Krishna Mohan on 04/12/18.
+//  Copyright © 2018 Krishna Mohan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocoFramework1.
+FOUNDATION_EXPORT double LocoFramework1VersionNumber;
+
+//! Project version string for LocoFramework1.
+FOUNDATION_EXPORT const unsigned char LocoFramework1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework1/PublicHeader.h>
+
+
---
+++ LocoFramework1/Service.swift
@@ -0,0 +1,21 @@
+//
+//  Service.swift
+//  LocoFramework1
+//
+//  Created by Krishna Mohan on 04/12/18.
+//  Copyright © 2018 Krishna Mohan. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    
+    private init() {}
+        
+        public static func doSomething() -> String{
+            return "Did somethig stuf"
+        }
+}
+
